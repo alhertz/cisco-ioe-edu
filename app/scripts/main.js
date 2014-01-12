47c695f8f9e87f72d1f6bccb7b8f16b56461a9ba
Alex Hertz <alexhertz@designbyah.com>
2014-01-12 12:12:53 -0600

    One full module complete and ready to duplicate

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,8 +4,18 @@ $(function(){
   });
 });
 $(document).ready(function(){
-  // $('#scenario-1 .img').delay(4000).removeClass('pulse');
   $( "#scenario-1 .content" ).fadeIn( 2500 );
-  $( ".navbar" ).fadeIn( 2500 );
-  $( "#scenario-1 .secondary-content" ).delay( 800 ).fadeIn( 2500 );
+  $( "#scenario-1 .secondary-content" ).delay( 2500 ).fadeIn( 2500 );
+
+  $( ".showTechnology" ).click(function() {
+    $( ".scenarios > nav").fadeOut();
+    $( ".technology" ).fadeIn( 'slow', function() {
+      // Animation complete
+      // $('.close').delay( 500 ).addClass('slideDown');
+      $( ".close" ).click(function() {
+        $('.technology').fadeOut();
+        $( ".scenarios > nav").fadeIn();
+      });
+    });
+  });
 });
