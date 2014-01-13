eeabb5f02e2152e30a036a6acfbbc14026d6c87d
Alex Hertz <alexhertz@designbyah.com>
2014-01-13 10:56:39 -0600

    All copy marked up. On the cross device tweaking

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,18 @@
 $(function(){
   $('#scenario-2').waypoint(function(direction) {
-    $( ".fadeIn" ).fadeIn( 2500 );
+    $( "#scenario-2 .content" ).fadeIn( 2500 );
+  });
+  $('#scenario-3').waypoint(function(direction) {
+    $( "#scenario-3 .content" ).fadeIn( 2500 );
+  });
+  $('#scenario-4').waypoint(function(direction) {
+    $( "#scenario-4 .content" ).fadeIn( 2500 );
+  });
+  $('#scenario-5').waypoint(function(direction) {
+    $( "#scenario-5 .content" ).fadeIn( 2500 );
+  });
+  $('#scenario-6').waypoint(function(direction) {
+    $( "#scenario-6 .content" ).fadeIn( 2500 );
   });
 });
 $(document).ready(function(){
@@ -9,13 +21,18 @@ $(document).ready(function(){
 
   $( ".showTechnology" ).click(function() {
     $( ".scenarios > nav").fadeOut();
-    $( event.target ).closest( "li")( ".technology" ).fadeIn( 'slow', function() {
-      // Animation complete
-      // $('.close').delay( 500 ).addClass('slideDown');
-      $( ".close" ).click(function() {
-        $('.technology').fadeOut();
-        $( ".scenarios > nav").fadeIn();
+    $(".content").fadeOut( 500, function() {
+      $( ".technology" ).fadeIn( 1500, function() {
+        // Animation complete
+        // $('.close').delay( 500 ).addClass('slideDown');
+        $( ".close" ).click(function() {
+          $('.technology').fadeOut( 500, function() {
+            $('.content').fadeIn( 1500 );
+            $( ".scenarios > nav").fadeIn();
+          });
+        });
       });
     });
+    
   });
 });
