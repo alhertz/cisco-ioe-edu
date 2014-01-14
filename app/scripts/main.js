b8d298bd99f19c43249f9d345f87870c19bab738
Alex Hertz <alexhertz@designbyah.com>
2014-01-14 16:01:32 -0600

    Change font size base on large screens

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,17 @@
 $(function(){
+    // JS lint is mad at me
     'use strict';
-
+    // Fade the introduction in all sexy like
     $( '#scenario-1 .content' ).fadeIn( 2500 );
+    // Fade the secondary content in all sexy like too
+    // But also wait a little bit
     $( '#scenario-1 .secondary-content' ).delay( 500 ).fadeIn( 2500, function() {
         // $( '#scenario-1 #arrow' ).delay( 500 ).show().addClass( 'animated slideInDown' );
         $( '#scenario-1 #scenario-link' ).delay().addClass( 'animated swing' );
     });
 
+    // Trigger display of content when
+    // scenario wrapper is 25% (of the viewport) away
     $('#scenario-2').waypoint(function() {
         $( '#arrow' ).fadeOut( 2500 );
         $( '#scenario-2 .content, .scenarios nav' ).fadeIn( 2500 );
@@ -15,7 +20,6 @@ $(function(){
         $( '#scenario-3 .content' ).fadeIn( 2500 );
     },{ offset: '25%' });
     $('#scenario-4').waypoint(function() {
-
         $( '#scenario-4 .content' ).fadeIn( 2500 );
     },{ offset: '25%' });
     $('#scenario-5').waypoint(function() {
@@ -25,6 +29,8 @@ $(function(){
         $( '#scenario-6 .content' ).fadeIn( 2500 );
     },{ offset: '25%' });
 
+    // Make the show technology button...umm
+    // (you guessed it) show technology.
     $( '.showTechnology' ).click(function() {
         // Hide the scenarios nav
         $( '.scenarios > nav').fadeOut();
