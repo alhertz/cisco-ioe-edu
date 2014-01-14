56bcf93c2a061dc8c6f06a058cdaae23fa80fdfb
Alex Hertz <alexhertz@designbyah.com>
2014-01-14 00:14:18 -0600

    Offset on waypoints. Style differention on technology

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,34 +10,32 @@ $(function(){
     $('#scenario-2').waypoint(function() {
         $( '#arrow' ).fadeOut( 2500 );
         $( '#scenario-2 .content, .scenarios nav' ).fadeIn( 2500 );
-    });
+    },{ offset: '25%' });
     $('#scenario-3').waypoint(function() {
         $( '#scenario-3 .content' ).fadeIn( 2500 );
-    });
+    },{ offset: '25%' });
     $('#scenario-4').waypoint(function() {
         $( '#scenario-4 .content' ).fadeIn( 2500 );
-    });
+    },{ offset: '25%' });
     $('#scenario-5').waypoint(function() {
         $( '#scenario-5 .content' ).fadeIn( 2500 );
-    });
+    },{ offset: '25%' });
     $('#scenario-6').waypoint(function() {
         $( '#scenario-6 .content' ).fadeIn( 2500 );
-    });
+    },{ offset: '25%' });
 
     $( '.showTechnology' ).click(function() {
         // Hide the scenarios nav
         $( '.scenarios > nav').fadeOut();
         // Find respective .content and hide it 
-        $(this).closest('section').find('.content').fadeOut( 500, function() {
-            // Find respective .technology and show it once animation completes
-            $(this).closest('section').find('.technology').fadeIn(1500, function() {
-                $( '.close' ).click(function() {
-                    $('.technology').fadeOut( 500, function() {
-                        $('.content').fadeIn( 1500 );
-                        $( '.scenarios > nav').fadeIn();
-                    });
+        // $(this).closest('section').find('.content').toggle( 500, function()
+        $(this).closest('section').find('.technology').fadeIn(500, function() {
+            $( '.close' ).click(function() {
+                $('.technology').fadeOut( 500, function() {
+                    $('.content').fadeIn( 1500 );
+                    $( '.scenarios > nav').fadeIn();
                 });
-            }); 
+            });
         });
     });
 });
