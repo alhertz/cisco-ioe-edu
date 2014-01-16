c06ca5efb6a11816fa8438b555fdbe48a4264891
Alex Hertz <alexhertz@designbyah.com>
2014-01-16 08:59:18 -0600

    Revert 334f297..667f1c8
    This rolls back to commit 334f29756a136095bc2c28f024e5ee4982444ff2.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,6 @@
 $(function(){
     // JS lint is mad at me
     'use strict';
-    
-    $('#scenario-1').css({'height':(($(window).height()))+'px'});
-    $(window).resize(function(){
-        $('#scenario-1').css({'height':(($(window).height()))+'px'});
-    });
-
     // Fade the introduction in all sexy like
     $( '#scenario-1 .content' ).fadeIn( 2500 );
     // Fade the secondary content in all sexy like too
@@ -16,10 +10,10 @@ $(function(){
     // Tooltip Event
     $('.scenarios nav a').hover(
         function () {
-            $(this).closest('a').find( '.tooltip' ).css( 'opacity', '1.0' );
-        },
+            $(this).closest('a').find( '.tooltip' ).css( "opacity", "1.0" );
+        }, 
         function () {
-            $(this).closest('a').find( '.tooltip' ).css( 'opacity', '0' );
+            $(this).closest('a').find( '.tooltip' ).css( "opacity", "0" );
         }
     );
 
@@ -29,7 +23,7 @@ $(function(){
     $('#scenario-2').waypoint(function() {
         $( '#arrow' ).fadeOut( 2500 );
         $( '#scenario-2 .content, .scenarios nav' ).fadeIn( 1500 );
-        $(this).closest('section').find( '.img-sm' ).addClass( 'fadeIn' );
+        $(this).closest('section').find( '.img-sm' ).addClass( 'slideRight' );
         $(this).closest('section').find( '.img-md' ).addClass( 'slideUp' );
         $(this).closest('section').find( '.img-lg' ).addClass( 'slideRight' );
         $(this).closest('section').find( '.col-md-7' ).addClass( 'slideLeft' );
@@ -37,30 +31,30 @@ $(function(){
     $('#scenario-3').waypoint(function() {
         $( '#scenario-3 .content' ).fadeIn( 2500 );
         $(this).closest('section').find( '.img-sm' ).addClass( 'slideRight' );
-        $(this).closest('section').find( '.img-md' ).addClass( 'fadeIn' );
-        $(this).closest('section').find( '.img-lg' ).addClass( 'hatch' );
-        $(this).closest('section').find( '.col-md-7' ).addClass( 'fadeIn' );
+        $(this).closest('section').find( '.img-md' ).addClass( 'slideUp' );
+        $(this).closest('section').find( '.img-lg' ).addClass( 'slideRight' );
+        $(this).closest('section').find( '.col-md-7' ).addClass( 'slideLeft' );
     },{ offset: '50%' });
     $('#scenario-4').waypoint(function() {
         $( '#scenario-4 .content' ).fadeIn( 2500 );
         $(this).closest('section').find( '.img-sm' ).addClass( 'slideRight' );
-        $(this).closest('section').find( '.img-md' ).addClass( 'fadeIn' );
-        $(this).closest('section').find( '.img-lg' ).addClass( 'bigEntrance' );
-        $(this).closest('section').find( '.col-md-7' ).addClass( 'hatch' );
+        $(this).closest('section').find( '.img-md' ).addClass( 'slideUp' );
+        $(this).closest('section').find( '.img-lg' ).addClass( 'slideRight' );
+        $(this).closest('section').find( '.col-md-7' ).addClass( 'slideLeft' );
     },{ offset: '50%' });
     $('#scenario-5').waypoint(function() {
         $( '#scenario-5 .content' ).fadeIn( 2500 );
         $(this).closest('section').find( '.img-sm' ).addClass( 'slideRight' );
         $(this).closest('section').find( '.img-md' ).addClass( 'slideUp' );
         $(this).closest('section').find( '.img-lg' ).addClass( 'slideRight' );
-        $(this).closest('section').find( '.col-md-7' ).addClass( 'bigEntrance' );
+        $(this).closest('section').find( '.col-md-7' ).addClass( 'slideLeft' );
     },{ offset: '50%' });
     $('#scenario-6').waypoint(function() {
         $( '#scenario-6 .content' ).fadeIn( 2500 );
-        $(this).closest('section').find( '.img-sm' ).addClass( 'hatch' );
+        $(this).closest('section').find( '.img-sm' ).addClass( 'slideRight' );
         $(this).closest('section').find( '.img-md' ).addClass( 'slideUp' );
-        $(this).closest('section').find( '.img-lg' ).addClass( 'bigEntrance' );
-        $(this).closest('section').find( '.col-md-7' ).addClass( 'fadeIn' );
+        $(this).closest('section').find( '.img-lg' ).addClass( 'slideRight' );
+        $(this).closest('section').find( '.col-md-7' ).addClass( 'slideLeft' );
     },{ offset: '50%' });
 
     // Make the show technology button...umm
@@ -69,7 +63,7 @@ $(function(){
         // Hide the scenarios nav
         $( '.scenarios > nav').fadeOut();
         // Find respective .content and hide it
-        $(this).closest('section').find('.content').fadeOut( 500 );
+        $(this).closest('section').find('.content').fadeOut( 500 ); 
         $(this).closest('section').find('.technology').fadeIn( 1000, function() {
             // Blur the content and then fade it back in slowly
             $( '.close' ).click(function() {
